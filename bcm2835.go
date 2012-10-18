ffa7fe140d602d4e93544d2039a6fa288884e9e1
Jan Andersson <jan.andersson@gmail.com>
2012-10-18 19:55:48 +0200

    Add some docs, error handling and changed uint8 to int

--- bcm2835.go
+++ bcm2835.go
@@ -1,7 +1,9 @@
+// Package bcm2835 provides functions for the bcm2835 as used in the Raspberry Pi
 package bcm2835
 
 // #include "bcm2835.h"
 import "C"
+import "errors"
 
 const (
   Low = 0
@@ -27,125 +29,146 @@ const (
   Pin26 = 7
 )
 
-func Init() {
-  C.bcm2835_init()
-
-  /*
-  if (C.bcm2835_init() != 1) {
-    return
+// Init initialise the library by opening /dev/mem and getting pointers to the
+// internal memory for BCM 2835 device registers. You must call this
+// (successfully) before calling any other functions in this library (except
+// SetDebug)
+func Init() (err error) {
+  if C.bcm2835_init() == 0 {
+    return errors.New("Init: failed")
   }
-  */
+  return
 }
 
-func SetDebug(debug uint8) {
+// Close closes the library, deallocating any allocaterd memory and closing
+// /dev/mem
+func Close() (err error) {
+  if C.bcm2835_close() == 0 {
+    return errors.New("Close: failed")
+  }
+  return
+}
+
+// SetDebug sets the debug level of the library.  A value of 1 prevents mapping
+// to /dev/mem, and makes the library print out what it would do, rather than
+// accessing the GPIO registers.  A value of 0, the default, causes normal
+// operation.  Call this before calling Init()
+func SetDebug(debug int) {
   C.bcm2835_set_debug(C.uint8_t(debug))
 }
 
-func Close() {
-  C.bcm2835_close()
-}
-
-func GpioFsel(pin, mode uint8) {
+// GpioFsel sets the function select register for the given pin, which
+// configures the pin as either Input or Output
+func GpioFsel(pin, mode int) {
   C.bcm2835_gpio_fsel(C.uint8_t(pin), C.uint8_t(mode))
 }
 
+/*
 func Delay(millis uint) {
   C.delay(C.uint(millis))
 }
+*/
 
-func GpioSet(pin uint8) {
+// GpioSet sets the specified pin output to high.
+func GpioSet(pin int) {
   C.bcm2835_gpio_set(C.uint8_t(pin))
 }
 
-func GpioClr(pin uint8) {
+// GpioClr sets the specified pin output to low.
+func GpioClr(pin int) {
   C.bcm2835_gpio_clr(C.uint8_t(pin))
 }
 
-func GpioLev(pin uint8) uint8 {
-  return uint8(C.bcm2835_gpio_lev(C.uint8_t(pin)))
+// GpioLev reads the current level on the specified pin and returns either high
+// or low. Works whether or not the pin is an input or an output.
+func GpioLev(pin int) int {
+  return int(C.bcm2835_gpio_lev(C.uint8_t(pin)))
 }
 
-func GpioEds(pin uint8) uint8 {
-  return uint8(C.bcm2835_gpio_eds(C.uint8_t(pin)))
+func GpioEds(pin int) int {
+  return int(C.bcm2835_gpio_eds(C.uint8_t(pin)))
 }
 
-func GpioSetEds(pin uint8) {
+func GpioSetEds(pin int) {
   C.bcm2835_gpio_set_eds(C.uint8_t(pin))
 }
 
-func GpioRen(pin uint8) {
+func GpioRen(pin int) {
   C.bcm2835_gpio_ren(C.uint8_t(pin))
 }
 
-func GpioClrRen(pin uint8) {
+func GpioClrRen(pin int) {
   C.bcm2835_gpio_clr_ren(C.uint8_t(pin))
 }
 
-func GpioFen(pin uint8) {
+func GpioFen(pin int) {
   C.bcm2835_gpio_fen(C.uint8_t(pin))
 }
 
-func GpioClrFen(pin uint8) {
+func GpioClrFen(pin int) {
   C.bcm2835_gpio_clr_fen(C.uint8_t(pin))
 }
 
-func GpioHen(pin uint8) {
+func GpioHen(pin int) {
   C.bcm2835_gpio_hen(C.uint8_t(pin))
 }
 
-func GpioClrHen(pin uint8) {
+func GpioClrHen(pin int) {
   C.bcm2835_gpio_clr_hen(C.uint8_t(pin))
 }
 
-func GpioLen(pin uint8) {
+func GpioLen(pin int) {
   C.bcm2835_gpio_len(C.uint8_t(pin))
 }
 
-func GpioClrLen(pin uint8) {
+func GpioClrLen(pin int) {
   C.bcm2835_gpio_clr_len(C.uint8_t(pin))
 }
 
-func GpioAren(pin uint8) {
+func GpioAren(pin int) {
   C.bcm2835_gpio_aren(C.uint8_t(pin))
 }
 
-func GpioClrAren(pin uint8) {
+func GpioClrAren(pin int) {
   C.bcm2835_gpio_clr_aren(C.uint8_t(pin))
 }
 
-func GpioAfen(pin uint8) {
+func GpioAfen(pin int) {
   C.bcm2835_gpio_afen(C.uint8_t(pin))
 }
 
-func GpioClrAfen(pin uint8) {
+func GpioClrAfen(pin int) {
   C.bcm2835_gpio_clr_afen(C.uint8_t(pin))
 }
 
-func GpioPud(pud uint8) {
+func GpioPud(pud int) {
   C.bcm2835_gpio_pud(C.uint8_t(pud))
 }
 
-func GpioPudclk(pin, on uint8) {
+func GpioPudclk(pin, on int) {
   C.bcm2835_gpio_pudclk(C.uint8_t(pin), C.uint8_t(on))
 }
 
-func GpioPad(group uint8) uint32 {
+func GpioPad(group int) uint32 {
   return uint32(C.bcm2835_gpio_pad(C.uint8_t(group)))
 }
 
-func GpioSetPad(group uint8, control uint32) {
+func GpioSetPad(group int, control uint32) {
   C.bcm2835_gpio_set_pad(C.uint8_t(group), C.uint32_t(control))
 }
 
+/*
 func DelayMicroseconds(micros uint) {
   C.delayMicroseconds (C.uint(micros))
 }
+*/
 
-func GpioWrite(pin, on uint8) {
+/// GpioWrite sets the output state of the specified pin
+func GpioWrite(pin, on int) {
   C.bcm2835_gpio_write(C.uint8_t(pin), C.uint8_t(on))
 }
 
-func GpioSetPud(pin, pud uint8) {
+func GpioSetPud(pin, pud int) {
   C.bcm2835_gpio_set_pud(C.uint8_t(pin), C.uint8_t(pud))
 }
 
@@ -157,7 +180,7 @@ func SpiEnd() {
   C.bcm2835_spi_end()
 }
 
-func SpiSetBitOrder(order uint8) {
+func SpiSetBitOrder(order int) {
   C.bcm2835_spi_setBitOrder(C.uint8_t(order))
 }
 
@@ -165,20 +188,20 @@ func SpiSetClockDivider(divider uint16) {
   C.bcm2835_spi_setClockDivider(C.uint16_t(divider))
 }
 
-func SpiSetDataMode(mode uint8) {
+func SpiSetDataMode(mode int) {
   C.bcm2835_spi_setDataMode(C.uint8_t(mode))
 }
 
-func SpiChipSelect(cs uint8) {
+func SpiChipSelect(cs int) {
   C.bcm2835_spi_chipSelect(C.uint8_t(cs))
 }
 
-func SpiSetChipSelectPolarity(cs, active uint8) {
+func SpiSetChipSelectPolarity(cs, active int) {
   C.bcm2835_spi_setChipSelectPolarity(C.uint8_t(cs), C.uint8_t(active))
 }
 
-func SpiTransfer(value uint8) uint8 {
-  return uint8(C.bcm2835_spi_transfer(C.uint8_t(value)))
+func SpiTransfer(value int) int {
+  return int(C.bcm2835_spi_transfer(C.uint8_t(value)))
 }
 
 /*
